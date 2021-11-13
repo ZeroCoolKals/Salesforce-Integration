5f474ac42471ce54937ba8e3e484d9e98fa3fb4f
ZeroCoolKals <kalsballer@gmail.com>
2021-11-13 14:17:05 +0000

    init project

---
+++ app.js
@@ -0,0 +1,6 @@
+var oauth2 = new jsforce.OAuth2({
+    loginUrl: 'https://login.salesforce.com',
+    clientId: '3MVG9fTLmJ60pJ5LpcrrTU5CNRfr6aKzThc8FvGIaHERJUtKK9AxyWOyB2WSoBcacIzxGwO1YNsQJRzubfBn.',
+    clientSecret: '9EF0E85A4F547C2111E1E5581BFA92E2603B8EDA0C33678F7E158CF6AB8D61E8',
+    redirectUri: 'https://sfdc-test-integration.herokuapp.com/'
+});
